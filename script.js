f123eed253387a9da83f2e51caa0c4a71cb2ebae
527elarock <527elarock@frhsd.com>
2026-02-20 10:42:11 -0500

    2/20

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-function pressNumber(digit) {
-    console.log(digit)
+//What the user is currently typing
+
+let typedNumberText = ''
+
+//Number we store for calculations
+let storedNumber= null
+
+//The operator currently selected 
+let currentOperator = ''
+
+//Used only for displaying history line
+let historyParts = []
+
+
+//Helper functions
+
+function setStatus(message){
+    document.getElementById('statusLine').textContent = message
+}
+
+
+
+function showSymbol(op){
+    if (op === '*') return '×'
+    if (op === '/') return '÷'
+    if (op === '-') return '&#x2212;'
+    return op
+}
+
+function updateScreen(){
+    const display = document.getElementById('displayLine')
+    const history = document.getElementById('historyLine')
+    const status = document.getElementById('statusLine')
+
+    display.textContent = typedNumberText
+}
+
+
+function pressNumber(digit){
+    
+
+    if (typedNumberText === '0'){
+        typedNumberText = digit;
+    } else {
+            typedNumberText = typedNumberText + digit
+
+    }
+    
+    
+
+    updateScreen()
 }
